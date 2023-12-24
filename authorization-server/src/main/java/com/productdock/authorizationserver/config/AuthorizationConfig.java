f82a3b60a83048055ebb2ae161883fe8e4dead34
Bojan Coric <bojancoric996@gmail.com>
2023-12-24 23:59:46 +0100

    Persist authorizations in DB

---
+++ authorization-server/src/main/java/com/productdock/authorizationserver/config/AuthorizationConfig.java
@@ -0,0 +1,25 @@
+package com.productdock.authorizationserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+
+@Configuration
+public class AuthorizationConfig {
+
+    @Bean
+    public JdbcOAuth2AuthorizationConsentService jdbcOAuth2AuthorizationConsentService(
+            JdbcOperations jdbcOperations, RegisteredClientRepository registeredClientRepository) {
+        return new JdbcOAuth2AuthorizationConsentService(jdbcOperations, registeredClientRepository);
+    }
+
+    @Bean
+    public JdbcOAuth2AuthorizationService jdbcOAuth2AuthorizationService(
+            JdbcOperations jdbcOperations,
+            RegisteredClientRepository registeredClientRepository) {
+        return new JdbcOAuth2AuthorizationService(jdbcOperations, registeredClientRepository);
+    }
+}
